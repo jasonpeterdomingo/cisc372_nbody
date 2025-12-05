378df3f294b587cbbd0abc4a2d59769a0703f0ee
jasonpeterdomingo <jayprdomingo16@gmail.com>
2025-12-04 22:27:11 -0500

    Debugging: fixed accuracy by fixing accelmag index and issues with launching kernels

--- compute.cu
+++ compute.cu
@@ -47,7 +47,7 @@ __global__ void computeAccels(vector3 *hPos, double *mass, vector3 *accels) {
             for (int k=0;k<3;k++) distance[k]=sharedPosI[threadIdx.y][k]-sharedPosJ[threadIdx.x][k];
             double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
             double magnitude=sqrt(magnitude_sq);
-            double accelmag=-1*GRAV_CONSTANT*sharedMassJ[j]/magnitude_sq;
+            double accelmag=-1*GRAV_CONSTANT*sharedMassJ[threadIdx.x]/magnitude_sq;
 
             for (int k = 0; k < 3; k++) {
                 accels[accelIndex][k] = accelmag * distance[k] / magnitude;
@@ -99,11 +99,11 @@ __global__ void getEffect(vector3 *hPos, vector3 *hVel, vector3 *accels) {
 // Compute function to launch kernels
 void compute() {
     dim3 dimBlock(BLOCK_DIM_XY, BLOCK_DIM_XY);
-    dim3 dimGrid((NUMENTITIES * BLOCK_DIM_XY - 1) / BLOCK_DIM_XY, (NUMENTITIES * BLOCK_DIM_XY - 1) / BLOCK_DIM_XY);
+    dim3 dimGrid((NUMENTITIES + BLOCK_DIM_XY - 1) / BLOCK_DIM_XY, (NUMENTITIES + BLOCK_DIM_XY - 1) / BLOCK_DIM_XY);
 
     computeAccels<<<dimGrid, dimBlock>>>(d_hPos, d_mass, d_accels);
 
-    int numBlocks = (NUMENTITIES + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+    int numBlocks = NUMENTITIES;  // one block per entity
     getEffect<<<numBlocks, THREADS_PER_BLOCK>>>(d_hPos, d_hVel, d_accels);
 
     cudaDeviceSynchronize();
